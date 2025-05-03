abc05e2fb4037b1554f02c627fbd27c14e1fde1e
MrTrustee <fakoredesemiloore@gmail.com>
2025-05-03 11:10:06 +0100

    Cool

--- .idea/Codewar.py
+++ .idea/Codewar.py
@@ -1,13 +1,13 @@
 first_team_name = input().split()
 second_team_number = input().split()
-for i in first_team:
-    s, y = map(int, input().split())
+for i in first_team_name:
+    s, y = map(str, input().split())
     if s != "A" and y != "B":
         print("None of them will be active on Monday or Tuesday")
     else:
         print("The Team will be active on Monday")
 
-for j in range(1, second_team):
+for j in second_team_number:
      first_attempt = int(input())
-     print(f"The attempt {j} for the team will be in ratio of ", 2 * j, ":", 1)
+     print(f"The name {j} for the team will be shown ", j)
 
