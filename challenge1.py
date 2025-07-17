2137f5e413e5596b62e449e26b18141c9a1e102e
mrtrustee <fakoredesemiloore@gmail.com>
2025-07-17 23:21:41 +0100

    cursor

--- challenge1.py
+++ challenge1.py
@@ -1,4 +1,9 @@
 #The challenge just to input the flag as already found in the instruction
+import base64
+input_ = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
+convert = bytes.fromhex(input_)
+attempt = base64.b64encode(convert)
+
 flag ="crypto{y0ur_f1rst_fl4g}"
 #for Ascii, to convert the number given
 list_ = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
@@ -7,4 +12,5 @@ for i in list_:
     conv = chr(i)
     then = then + conv
 print(then)
+print(attempt)
 
